ec565761f7cfeb2e39baf6dcf5ac1fd50465a339
yumin359 <yumin359@gmail.com>
2025-07-01 15:46:49 +0900

    0701 react bootstrap modal

--- src/App18.jsx
+++ src/App18.jsx
@@ -1,9 +1,38 @@
-import { Container, Nav, Navbar, Pagination, Spinner } from "react-bootstrap";
+import {
+  Button,
+  Container,
+  Modal,
+  Nav,
+  Navbar,
+  Pagination,
+  Spinner,
+} from "react-bootstrap";
+import { useState } from "react";
 import { GrCaretNext, GrCaretPrevious } from "react-icons/gr";
 
 function App18() {
+  const [show, setShow] = useState(false);
   return (
     <div>
+      <Button onClick={() => setShow(!show)}>모달 예제</Button>
+      <Modal show={show} onHide={() => setShow(!show)}>
+        <Modal.Header closeButton={true}>
+          <Modal.Title>Lorem ipsum dolor sit amet.</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio
+          doloremque ducimus laborum magnam qui quos ratione saepe voluptatibus
+          voluptatum! Aliquam aspernatur beatae consectetur harum in magni
+          maxime mollitia odio optio?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShow(!show)}>
+            Close
+          </Button>
+          <Button onClick={() => setShow(!show)}>Save</Button>
+        </Modal.Footer>
+      </Modal>
+      <hr />
       <Navbar className="bg-body-tertiary" expand="lg">
         <Container>
           <Navbar.Brand>PROJECT</Navbar.Brand>
