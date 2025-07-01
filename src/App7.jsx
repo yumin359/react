890ea73e6be3799a646647dfeac0b40d94acc210
yumin359 <yumin359@gmail.com>
2025-07-01 11:19:35 +0900

    0701 props의 타입은 뭐든지 가능하고 {} 사용, string 만 {} 생략 가능

---
+++ src/App7.jsx
@@ -0,0 +1,39 @@
+function MyComp1({ color, score, city, kids, skills, person }) {
+  console.log("color", color);
+  console.log("score", score);
+  console.log("city", city);
+  console.log("kids", kids);
+  console.log("skills", skills);
+  console.log("person", person);
+  return null;
+}
+
+function MyComp2({ name, age, address, married }) {
+  console.log("name", name); // lee
+  console.log("age", age); // 44
+  console.log("address", address); // {city: "seoul", country: "korea"}
+  console.log("married", married); // false
+  return null;
+}
+
+// props 의 타입은 뭐든지 가능
+// string은 "", {""}도 가능은 함
+// 나머지는 {}
+function App7() {
+  return (
+    <>
+      <MyComp1
+        color="yellow"
+        score={123.123}
+        city={"seoul"}
+        kids={true}
+        skills={["java", "css", "react"]}
+        person={{ name: "son", age: 33, team: "토트넘" }}
+      />
+      {/* 연습 : props 작성해보기 */}
+      <MyComp2 />
+    </>
+  );
+}
+
+export default App7;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App6 from "./App6.jsx";
+import App7 from "./App7.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App6 />
+    <App7 />
   </StrictMode>,
 );
