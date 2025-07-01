e72ab8fc6b94afd3f3317795c60507b1929174b3
yumin359 <yumin359@gmail.com>
2025-07-01 10:51:25 +0900

    0701 props(property), 분해할당 기본값

--- src/App6.jsx
+++ src/App6.jsx
@@ -26,6 +26,19 @@ function MyComp4({ city }) {
   return null;
 }
 
+function MyComp5({ city, country, address = "기본 주소" }) {
+  console.log("도시5", city, "나라5", country);
+  console.log("주소5", address);
+  return null;
+}
+
+function MyComp6({ score, className, location = "기본 위치" }) {
+  console.log("점수6", score);
+  console.log("이름6", className);
+  console.log("위치6", location);
+  return null;
+}
+
 function App6() {
   return (
     <>
@@ -40,6 +53,13 @@ function App6() {
       <MyComp4 city="서울" />
       <MyComp4 city="워싱턴" />
       {/* MyComp4 함수 작성/ 분해 할당으로 props 받기 */}
+      <MyComp5 city="인천" country="한국" address="강남" />
+      <MyComp5 city="텍사스" country="미국" address="라스베가스" />
+      <MyComp5 city="오사카" country="일본" />
+      {/* 연습 : MyComp6이 받은 props 들 출력, location의 기본값은 "기본위치" */}
+      <MyComp6 score="A" className="중앙" location="신촌" />
+      <MyComp6 score="B" className="센터" location="강남" />
+      <MyComp6 score="C" className="기린" />
     </>
   );
 }
