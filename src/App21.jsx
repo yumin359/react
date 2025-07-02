61008b0f3f0801eda20f35b2aea5036891d01a6e
yumin359 <yumin359@gmail.com>
2025-07-02 10:26:11 +0900

    0702 이벤트 버블링

---
+++ src/App21.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function App21(props) {
+  function handleButtonClick(e) {
+    // e : event 객체
+    console.log(e);
+
+    // stopPropagation() : 이벤트 전달 막기
+    e.stopPropagation();
+    console.log("button click");
+  }
+
+  return (
+    <div className="p-5 bg-info" onClick={() => console.log("div click")}>
+      {/* 연습 : p 클릭 이벤트가 div까지 전파되지 않도록 코드 수정 */}
+      <p
+        className="bg-danger"
+        onClick={(e) => {
+          e.stopPropagation();
+          console.log("para click");
+        }}
+      >
+        Lorem ipsum dolor.
+      </p>
+
+      <button onClick={handleButtonClick}>버튼</button>
+    </div>
+  );
+}
+
+export default App21;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App20 from "./App20.jsx";
+import App21 from "./App21.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <>
-      <App20 />
+      <App21 />
     </>
   </StrictMode>,
 );
