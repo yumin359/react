33b2ac05d1cd4d7d682082b8bf878875669ab0fa
yumin359 <yumin359@gmail.com>
2025-07-04 08:55:54 +0900

    색깔

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,29 @@ function App() {
   return (
     <>
       <p>첫번째 리액트 코드</p>
+      {/*인스타 하이라이트 색상*/}
+      <div style={{ backgroundColor: "#FF2D75", height: "100px" }}>.</div>
+      {/* 핑크 */}
+      <div style={{ backgroundColor: "#FFD600", height: "100px" }}>.</div>
+      {/* 노랑 */}
+      <div style={{ backgroundColor: "#00C851", height: "100px" }}>.</div>
+      {/* 초록 */}
+      <div style={{ backgroundColor: "#5AC8FA", height: "100px" }}>.</div>
+      {/* 파랑 */}
+      <div style={{ backgroundColor: "#C084FC", height: "100px" }}>.</div>
+      {/* 보라 */}
+      <hr />
+      {/*파스텔톤*/}
+      <div style={{ backgroundColor: "#FF8FA8", height: "100px" }}>.</div>
+      {/* 핑크 */}
+      <div style={{ backgroundColor: "#FFF59D", height: "100px" }}>.</div>
+      {/* 노랑 */}
+      <div style={{ backgroundColor: "#A0EAC7", height: "100px" }}>.</div>
+      {/* 초록 */}
+      <div style={{ backgroundColor: "#AEE4FA", height: "100px" }}>.</div>
+      {/* 파랑 */}
+      <div style={{ backgroundColor: "#E3C9FF", height: "100px" }}>.</div>
+      {/* 보라 */}
     </>
   );
 }
