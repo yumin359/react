ee5901a281a84d2d6509de0468cfbf74f8d09214
yumin359 <yumin359@gmail.com>
2025-07-03 09:26:01 +0900

    0703 객체 내의 또 다른 객체가 있다면 깊은 복사를 해야함

---
+++ src/App32.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+
+function MyComp1() {
+  const [person, setPerson] = useState({
+    name: "",
+    address: { city: "", country: "" },
+  });
+  const [history, setHistory] = useState([]);
+
+  function handleNameChange(e) {
+    const nextPerson = structuredClone(person);
+    nextPerson.name = e.target.value;
+    setPerson(nextPerson);
+  }
+  function handleCityChange(e) {
+    const nextPerson = structuredClone(person);
+    nextPerson.address.city = e.target.value;
+    setPerson(nextPerson);
+  }
+  function handleCountryChange(e) {
+    const nextPerson = structuredClone(person);
+    nextPerson.address.country = e.target.value;
+    setPerson(nextPerson);
+  }
+  function handleSaveClick() {
+    const nextHistory = [...history];
+    nextHistory.push(person);
+    setHistory(nextHistory);
+  }
+
+  return (
+    <div>
+      {/*div.d-flex>div.flex-1*2*/}
+      <div className="d-flex">
+        <div className="flex-1">
+          <div>
+            <input
+              type="text"
+              value={person.name}
+              onChange={handleNameChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              value={person.address.city}
+              onChange={handleCityChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              value={person.address.country}
+              onChange={handleCountryChange}
+            />
+          </div>
+          <div>
+            <button onClick={handleSaveClick}>변경</button>
+          </div>
+        </div>
+        <div className="flex-1">
+          <h3>변경 이력</h3>
+          {history.map((item) => (
+            <div className="border my-1">
+              <div>이름 : {item.name}</div>
+              <div>도시 : {item.address.city}</div>
+              <div>국가 : {item.address.country}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MyComp2() {
+  return null;
+}
+
+function App32(props) {
+  return (
+    <div>
+      {/*잘 안 돌아가는 버전(얕은 복사 사용)*/}
+      <MyComp2 />
+      <hr />
+      {/*잘 돌아가는 버전(깊은 복사 사용)*/}
+      <MyComp1 />
+    </div>
+  );
+}
+
+export default App32;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App31 from "./App31.jsx";
+import App32 from "./App32.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <>
-      <App31 />
+      <App32 />
     </>
   </StrictMode>,
 );
