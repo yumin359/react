2dcd8f3f1780e0a5fd32df2e4ff094da757f9999
yumin359 <yumin359@gmail.com>
2025-07-04 08:55:42 +0900

    색깔

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App37 from "./App37.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode> // 나중에 설명 // 콘솔에 두번씩 나오는 거 이거 없애니까 일단 사라짐
-  <App37 />,
+  <App />,
   // </StrictMode>,
 );
