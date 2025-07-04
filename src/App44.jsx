96d5bccbaec7aaa5f6b81059ae53f06cb4af1712
yumin359 <yumin359@gmail.com>
2025-07-04 10:50:57 +0900

    0704 nested routes(중첩된 경로)

---
+++ src/App44.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+// 연습 : 아래 경로에 따라 해당 컴포넌트 출력하기
+// /learn/react : <h3>리액트 배우기</h3>
+// /learn/spring : <h3>스프링 배우기</h3>
+// /learn/java/collection : <h3>자바 콜렉션 배우기</h3>
+
+function App44(props) {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="learn">
+            <Route path="react" element={<h3>learn react</h3>}></Route>
+            <Route path="spring" element={<h3>learn spring</h3>}></Route>
+            <Route
+              path="java/collection"
+              element={<h3>learn java collection</h3>}
+            ></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App44;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App43 from "./App43.jsx";
+import App44 from "./App44.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App43 />
+    <App44 />
   </StrictMode>,
 );
