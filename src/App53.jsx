ea1b1848a2a38017146c1fbd7dc5e8154c1f4d8e
yumin359 <yumin359@gmail.com>
2025-07-07 09:41:37 +0900

    0707 get,post 요청 보내기

--- src/App53.jsx
+++ src/App53.jsx
@@ -2,24 +2,34 @@ import React from "react";
 import axios from "axios";
 
 function App53(props) {
-  function handleButtonClick() {
+  function handleButton1Click() {
     axios.request({
-      method: "GET",
+      method: "get",
       url: "http://localhost:8080/main1/sub1",
     });
   }
 
   function handleButton2Click() {
     axios.request({
-      method: "GET",
-      url: "/api/main1/sub1",
+      method: "get",
+      url: "/api/main28/sub1",
+    });
+  }
+
+  function handleButton3Click() {
+    axios.request({
+      method: "post",
+      url: "/api/main28/sub2",
     });
   }
 
   return (
     <div>
-      <button onClick={handleButton2Click}>요청 보내기 2</button> <hr />
-      <button onClick={handleButtonClick}>요청 보내기 1</button>{" "}
+      <button onClick={handleButton3Click}>요청 보내기 3</button>
+      <hr />
+      <button onClick={handleButton2Click}>요청 보내기 2</button>
+      <hr />
+      <button onClick={handleButton1Click}>요청 보내기 1</button>
     </div>
   );
 }
