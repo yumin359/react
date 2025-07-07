ee49b75583d6cc5c226f871a4ef8447589b136ff
yumin359 <yumin359@gmail.com>
2025-07-07 16:43:56 +0900

    0707 send file(s) 내일 다시

--- src/App55.jsx
+++ src/App55.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function App55(props) {
+  const [myFile, setMyFile] = useState(null);
+
   function handleButton1Click() {
     axios.put("/api/main30/sub1");
   }
@@ -46,8 +48,27 @@ function App55(props) {
   // 연습 : spring delete /api/main30/sub6 으로 오는 요청
   // request parameter 받아서 출력하는 코드 완성
 
+  function handleButton7Click() {
+    // file 전송시 postForm 또는 putForm
+    axios.postForm("/api/main30/sub7", {
+      name: "흥민",
+      age: 77,
+      myFile: myFile,
+    });
+  }
+
   return (
     <div>
+      <div>
+        <input
+          type="file"
+          value={myFile}
+          onChange={(e) => setMyFile(e.target.value)}
+        />
+        <button onClick={handleButton7Click}>post 7 w/ fule</button>
+      </div>
+      <hr />
+
       <button onClick={handleButton6Click}>delete 6 w/ data</button>
       <hr />
       <button onClick={handleButton5Click}>put 5 w/ data</button>
