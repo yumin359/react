b25c15be949509942f8186a1e5ab248e349a50db
yumin359 <yumin359@gmail.com>
2025-07-07 12:31:30 +0900

    0707 post w/ json data

--- src/App54.jsx
+++ src/App54.jsx
@@ -17,8 +17,26 @@ function App54(props) {
     axios.post("/api/main29/sub3");
   }
 
+  function handleButton4Click() {
+    // 두번째 파라미터는 json으로 직렬화(stringify, encoding)
+    axios.post("/api/main29/sub4", { name: "son", age: 44 });
+  }
+
+  function handleButton5Click() {
+    // 연습 : 적절한 코드 작성
+    axios.post("/api/main29/sub5", {
+      address: "seoul",
+      score: 88.12,
+      country: "korea",
+    });
+  }
+
   return (
     <div>
+      <button onClick={handleButton5Click}>post 5 w/ data</button>
+      <hr />
+      <button onClick={handleButton4Click}>post 4 w/ data</button>
+      <hr />
       <button onClick={handleButton3Click}>post 3</button>
       <hr />
       <button onClick={handleButton2Click}>post 2</button>
