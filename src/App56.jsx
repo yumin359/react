75b54a97a9e1c1c8f6c0a67f39b66018976a62fa
yumin359 <yumin359@gmail.com>
2025-07-08 09:31:05 +0900

    0708 파일 보내기(하나, 여러개)

---
+++ src/App56.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App56(props) {
+  const [myFiles, setMyFiles] = useState(null);
+
+  function handleButton1Click() {
+    // file 전송시 postForm/putForm
+    axios.postForm("api/main31/sub1", {
+      name: "흥민",
+      age: 77,
+      myFiles: myFiles,
+    });
+  }
+
+  function handleButton2Click() {
+    axios.postForm("api/main31/sub2", {
+      address: "신촌",
+      score: 55.66,
+      yourFiles: myFiles,
+    });
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="file"
+          multiple={true}
+          onChange={(e) => setMyFiles(e.target.files)}
+        />
+        <button onClick={handleButton2Click}>파일 전송 2 여러개</button>
+      </div>
+      <hr />
+      <div>
+        {/* file은 e.target.files */}
+        <input type="file" onChange={(e) => setMyFiles(e.target.files)} />
+        <button onClick={handleButton1Click}>파일 전송 1</button>
+      </div>
+    </div>
+  );
+}
+
+export default App56;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App55 from "./App55.jsx";
+import App56 from "./App56.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App55 />
+    <App56 />
   </StrictMode>,
 );
