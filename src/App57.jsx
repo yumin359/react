9f0dc89dd9e4f98a5bfe3566828ba113c2ca9829
yumin359 <yumin359@gmail.com>
2025-07-08 12:35:20 +0900

    0708 상태 코드별 코드 실행

--- src/App57.jsx
+++ src/App57.jsx
@@ -178,8 +178,29 @@ function App57(props) {
       });
   }
 
+  function handleButton23Click() {
+    axios
+      .get("/api/main32/sub23")
+      .then((res) => {
+        console.log("잘됨");
+        console.log(res.status);
+      })
+      .catch((err) => {
+        console.log("잘안됨");
+        console.log(err.response.status);
+        if (err.response.status === 400) {
+          console.log("클라이언트 잘못");
+        } else if (err.response.status === 500) {
+          console.log("서버 잘못");
+        }
+      });
+  }
+
   return (
     <div>
+      <button onClick={handleButton23Click}>23 / 200, 400, 500 확률적</button>
+      <hr />
+
       <button onClick={handleButton21Click}>21 / 201번 w/ data</button>
       <button onClick={handleButton22Click}>22 / 500번 w/ data</button>
       <hr />
